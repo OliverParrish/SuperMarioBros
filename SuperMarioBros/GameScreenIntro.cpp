b0dc598567625ba01ca013585cb12eac7d5378e9
Spacemonkeey <55544034+Spacemonkeey@users.noreply.github.com>
2020-02-24 21:32:10 +0000

    finished Title Screen

--- SuperMarioBros/GameScreenIntro.cpp
+++ SuperMarioBros/GameScreenIntro.cpp
@@ -1,11 +1,16 @@
 #include "GameScreenIntro.h"
-#include "Source.cpp"
+#include <iostream>
 
 
 
 GameScreenIntro::GameScreenIntro(SDL_Renderer* renderer) : GameScreen(renderer)
 {
-
+	mRenderer = renderer;
+	mBackgroundTexture = new Texture2D(mRenderer);
+	if (!mBackgroundTexture->LoadFromFile("Images/IntroScreen.png"))
+	{
+		std::cout << "failed to load background texture!" << std::endl;
+	}
 }
 
 GameScreenIntro::~GameScreenIntro()
@@ -17,12 +22,8 @@ GameScreenIntro::~GameScreenIntro()
 void GameScreenIntro::Render()
 {
 	mBackgroundTexture->Render(Vector2D(0, 0), SDL_FLIP_NONE);
-
-	if (!mBackgroundTexture->LoadFromFile("Images/IntroScreen.png"))
-	{
-		std::cout << "failed to load background texture!" << std::endl;
-	}
 }
+	
 
 void GameScreenIntro::Update(float deltaTime, SDL_Event e)
 {
